f09909d6b661d3f4bfa268b5b0e5ed479141d5dc
Sylvain <sylvainferrer.lewagon@gmail.com>
2023-08-02 09:44:48 +0000

    Modified sales.sql

--- models/sales.sql
+++ models/sales.sql
@@ -10,21 +10,16 @@ SELECT
   s.date_date
   ### Key ###
   ,s.orders_id
-  --- descriptions: pk for orders ----
   ,s.pdt_id AS products_id
-  --- descriptions: pk for product reference ---- 
   ###########
 	-- qty --
 	,s.quantity AS qty
   -- revenue --
   ,s.revenue AS turnover
-  --- description: turnover with promos ---
   -- cost --
   ,CAST(p.purchSE_PRICE AS FLOAT64) AS purchase_price
 	,ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS purchase_cost
-    --- description: total purchase cost for each product id ------
 	-- margin --
-	,s.revenue - s.quantity*CAST(p.purchSE_PRICE AS FLOAT64) AS product_margin
-    --- description: margin =total turnover - purchase costs 
+	,ROUND(s.revenue-s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS margin
 FROM sales s
 INNER JOIN product p ON s.pdt_id = p.products_id
