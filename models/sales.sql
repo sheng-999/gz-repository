0629bb2d213c6fc3e2de8498e37a0ee3b9a3011c
Sheng <shengli0709@gmail.com>
2023-08-02 10:23:31 +0000

    17

--- models/sales.sql
+++ models/sales.sql
@@ -1,25 +1,25 @@
-WITH
+{{ config(schema='transaction') }}
+
+WITH 
+
   sales AS (SELECT * FROM `gz_raw_data.raw_gz_sales`)
+
   ,product AS (SELECT * FROM `gz_raw_data.raw_gz_product`)
+
 SELECT
   s.date_date
   ### Key ###
   ,s.orders_id
-  --- descriptions: pk for orders ----
   ,s.pdt_id AS products_id
-  --- descriptions: pk for product reference ----
   ###########
-  -- qty --
-  ,s.quantity AS qty
+	-- qty --
+	,s.quantity AS qty
   -- revenue --
   ,s.revenue AS turnover
-  --- description: turnover with promos ---
   -- cost --
   ,CAST(p.purchSE_PRICE AS FLOAT64) AS purchase_price
-  ,ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS purchase_cost
-    --- description: total purchase cost for each product id ------
-  -- margin --
-  ,s.revenue - s.quantity*CAST(p.purchSE_PRICE AS FLOAT64) AS margin
-    --- description: margin =total turnover - purchase costs
+	,ROUND(s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS purchase_cost
+	-- margin --
+	,ROUND(s.revenue-s.quantity*CAST(p.purchSE_PRICE AS FLOAT64),2) AS margin
 FROM sales s
 INNER JOIN product p ON s.pdt_id = p.products_id
